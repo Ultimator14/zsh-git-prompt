b66869176782f5b0c4d316d7ef031e69ebdcaea5
Jan Breig <jan.breig@freenet.de>
2020-12-07 03:16:13 +0100

    Set default executable to haskell

--- zshrc.zsh
+++ zshrc.zsh
@@ -2,7 +2,7 @@
 # # http://github.com/olivierverdier/zsh-git-prompt
 
 export __GIT_PROMPT_DIR=${0:A:h}
-export GIT_PROMPT_EXECUTABLE=${GIT_PROMPT_EXECUTABLE:-"python"}
+export GIT_PROMPT_EXECUTABLE=${GIT_PROMPT_EXECUTABLE:-"haskell"}
 
 # Initialize colors.
 autoload -U colors
